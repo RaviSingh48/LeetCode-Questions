98e2c502c3aeb5e2d584edbb6897c20e955d1bae
Ravi Singh <116999956+RaviSingh48@users.noreply.github.com>
2025-02-10 11:58:22 +0530

    Time: 1 ms (100%), Space: 42.7 MB (60.2%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder stack = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                if (stack.length() > 0) {
+                    stack.deleteCharAt(stack.length() - 1);
+                }
+            } else {
+                stack.append(c);
+            }
+        }
+        return stack.toString();
+    }
+}
